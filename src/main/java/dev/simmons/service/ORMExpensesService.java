dc9d53a375c457c29eb26134fe028dfeeaf28335
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-04-29 13:35:05 -0400

    Finished fleshing out the ORMService. Still dealing with a couple of bugs, but they seem like they might be related to not throwing the expected errors. Will continue Monday.

--- src/main/java/dev/simmons/service/ORMExpensesService.java
+++ src/main/java/dev/simmons/service/ORMExpensesService.java
@@ -9,6 +9,7 @@ import dev.simmons.utilities.logging.Logger;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ORMExpensesService implements ExpensesService{
     protected PostgresORM<Employee> empORM;
@@ -109,36 +110,140 @@ public class ORMExpensesService implements ExpensesService{
 
     @Override
     public List<Expense> getExpensesByStatus(Expense.Status status) {
-        return null;
+        List<Expense> expenses;
+
+        try {
+            expenses = expORM.getAllEntities();
+            if (expenses == null) {
+                return new ArrayList<>();
+            }
+
+            expenses = expenses.stream().
+                    filter(e -> e.getStatus().equals(status)).
+                    collect(Collectors.toList());
+        } catch (SQLException se) {
+            expenses = new ArrayList<>();
+        }
+        return expenses;
     }
 
     @Override
     public List<Expense> getExpensesByEmployee(int employeeId) {
-        return null;
+        List<Expense> expenses;
+
+        try {
+            expenses = expORM.getAllEntities();
+            if (expenses == null) {
+                return new ArrayList<>();
+            }
+
+            expenses = expenses.stream().
+                    filter(e -> e.getIssuer() == employeeId).
+                    collect(Collectors.toList());
+        } catch (SQLException se) {
+            expenses = new ArrayList<>();
+        }
+        return expenses;
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        List<Employee> employees;
+
+        try {
+            employees = empORM.getAllEntities();
+        } catch (SQLException se) {
+            employees = new ArrayList<>();
+        }
+
+        return employees;
     }
 
     @Override
     public Employee replaceEmployee(Employee employee) {
-        return null;
+        if (employee.getFirstName() == null || employee.getLastName() == null ||
+                employee.getFirstName().equals("") || employee.getLastName().equals("")) {
+            Logger.log(Logger.Level.WARNING, "Attempt to replace an employee with invalid first/last name.");
+            throw new InvalidEmployeeException();
+        }
+
+        Employee emp = null;
+        try {
+            emp = empORM.replaceEntity(employee);
+        } catch (SQLException se) {
+            Logger.log(Logger.Level.ERROR, se);
+        }
+
+        return emp;
     }
 
     @Override
     public Expense replaceExpense(Expense expense) {
-        return null;
+        if (expense.getStatus() == null) {
+            Logger.log(Logger.Level.WARNING, "Null/misspelled status was passed in to replace expense.");
+            throw new InvalidExpenseStatusException("Unable to parse the status passed in. Check for typos.");
+        }
+        if (expense.getIssuer() <= 0) {
+            Logger.log(Logger.Level.WARNING, "Attempt to submit an expense with no issuer assigned.");
+            throw new InvalidExpenseException("Unable to submit an expense not yet assigned an issuer.");
+        }
+        if (expense.getAmount() <= 0) {
+            Logger.log(Logger.Level.WARNING, "Attempt to submit an expense with a negative amount.");
+            throw new NonpositiveExpenseException(expense.getAmount());
+        }
+
+        Expense exp = null;
+        try {
+            exp = expORM.replaceEntity(expense);
+        } catch (SQLException se) {
+            if (se.getSQLState().equals("23503")) {
+                throw new NoSuchEmployeeException(expense.getIssuer());
+            } else if (se.getSQLState().equals("P0001")) {
+                Logger.log(Logger.Level.WARNING, "Attempt to replace contents of non-pending expense.");
+                throw new ExpenseNotPendingException(expense.getId());
+            }
+        }
+
+        return exp;
     }
 
     @Override
     public boolean deleteEmployee(int id) {
+        try {
+            List<Expense> expenses = getExpensesByEmployee(id);
+
+            if (expenses != null) {
+                int nonpendingCount = expenses.stream().mapToInt(e -> (e.getStatus().equals(Expense.Status.PENDING)) ? 0 : 1).reduce(0, (i,j) -> i+j);
+                if (nonpendingCount > 0) {
+                    Logger.log(Logger.Level.WARNING, "Attempt to delete employee matching (id: " + id + ") which has non-pending expense requests.");
+                    throw new EmployeeExpenseNotPendingException();
+                }
+
+                for (Expense e : expenses) {
+                    deleteExpense(e.getId());
+                }
+            }
+
+            return empORM.deleteEntity(id);
+        } catch (SQLException se) {
+            Logger.log(Logger.Level.ERROR, se);
+        }
         return false;
     }
 
     @Override
     public boolean deleteExpense(int id) {
+        try {
+            return expORM.deleteEntity(id);
+        } catch (SQLException se) {
+            if (se.getSQLState().equals("P0001")) {
+                Logger.log(Logger.Level.WARNING, "Attempt to delete non-pending expense.");
+                throw new ExpenseNotPendingException(id);
+            }
+
+            Logger.log(Logger.Level.ERROR, se);
+        }
+
         return false;
     }
 }
